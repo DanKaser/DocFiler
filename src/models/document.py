5c2ca42b30e72256d1562252de7c3d0c12e15c5e
TrueCoffeeMan <itry@gmail.com>
2025-10-18 10:11:07 +0200

    added info tag functionality for documents

--- src/models/document.py
+++ src/models/document.py
@@ -3,7 +3,7 @@ from sqlalchemy import UniqueConstraint, ForeignKey
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from src.db import Base
-from src.models.tags import TagDocType
+from src.models.tags import TagDocType, TagInfo, TaginfoXDocument
 
 class Document(Base):
 
@@ -18,6 +18,9 @@ class Document(Base):
                                              nullable=False)
     doc_type: Mapped[TagDocType] = relationship()
 
+    info_tags: Mapped[list[TagInfo]] = relationship(
+        "TagInfo", secondary = TaginfoXDocument)
+
     @property
     def abs_path(self) -> str:
         return os.path.join(os.getcwd(), self.rel_path, self.filename)
@@ -25,3 +28,9 @@ class Document(Base):
     @property
     def name(self) -> str:
         return os.path.split(self.filename)[0]
+
+    def add_info_tag(self, tag: TagInfo) -> None:
+        self.info_tags.append(tag) # TODO: check is TagInfo is in DB!
+
+    def rmv_info_tag(self, tag: TagInfo) -> None:
+        self.info_tags.remove(tag)
--- tests/test_db.py
+++ tests/test_db.py
@@ -12,7 +12,20 @@ class TestDB(unittest.TestCase):
         setup_db_con(TEST_DB_PATH, Base)
         DBConnSingleton()
 
+    def reset_db(self) -> None:
+        conn = DBConnSingleton().get_connection()
+        Base.metadata.drop_all(bind=conn)
+        Base.metadata.create_all(bind=conn)
+        conn.close()
+
     def setUp(self):
+        self.DOCNAME = 'test_doc'
+        self.DOCPATH = 'test/path'
+        self.TYPENAME = 'Test Type'
+        self.DOCNAME = 'test.pdf'
+        self.INFONAME = 'test_info'
+
+        self.reset_db()
         self.session = DBConnSingleton().get_session()
 
     def tearDown(self):
@@ -20,19 +33,35 @@ class TestDB(unittest.TestCase):
         self.session.close()
 
     def test_create_doc_and_tag(self) -> None:
-        DOCNAME = 'test.pdf'
-        TYPENAME = 'Test Type'
-        doctype = TagDocType(name = TYPENAME)
-        doc = Document(filename = DOCNAME, rel_path = 'rel/path',
+        doctype = TagDocType(name = self.TYPENAME)
+        doc = Document(filename = self.DOCNAME, rel_path = 'rel/path',
                        doc_type = doctype)
         self.session.add(doc)
         self.session.commit()
 
         db_doc = (self.session.query(Document)
-                              .where(Document.filename == DOCNAME)
+                              .where(Document.filename == self.DOCNAME)
                               .one())
-        self.assertEqual(db_doc.filename, DOCNAME, 'Doc name not matching.')
-        self.assertEqual(db_doc.doc_type.name, TYPENAME, 'Type name not matching')
+        self.assertEqual(db_doc.filename, self.DOCNAME, 'Doc name not matching.')
+        self.assertEqual(db_doc.doc_type.name, self.TYPENAME, 'Type name not matching')
+
+    def test_doc_info_interaction(self) -> None:
+        self.session.add(TagInfo(name = self.INFONAME))
+        self.session.add(Document(filename = self.DOCNAME,
+                                  rel_path = self.DOCPATH,
+                                  doc_type = TagDocType(name = self.TYPENAME)))
+        self.session.commit()
+
+        doc = (self.session.query(Document)
+                   .where(Document.rel_path == self.DOCPATH)
+                   .where(Document.filename == self.DOCNAME).one())
+        tag_info = (self.session.query(TagInfo)
+                    .where(TagInfo.name == self.INFONAME)
+                    .one())
+        doc.add_info_tag(tag_info)
+        self.assertListEqual(doc.info_tags, [tag_info])
+        doc.rmv_info_tag(tag_info)
+        self.assertListEqual(doc.info_tags, [])
 
 if __name__ == '__main__':
     unittest.main()
